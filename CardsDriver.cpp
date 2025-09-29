dc5b3db70b5c47d89bb8bdb6a8ace871c369850b
Rojan Nessari <rojannessari@Rojans-MacBook-Air.local>
2025-09-29 15:01:42 -0400

    q

--- CardsDriver.cpp
+++ CardsDriver.cpp
@@ -6,7 +6,12 @@ using namespace std;
 int main(){
     DebugPrint("Starting CardsDriver.cpp");
 Card c1(CardType::Airlift);
+Card c2=c1; // Copy constructor
+Card c3(CardType::Bomb);
+c3=c1; // Assignment operator
 cout<<c1<<endl;
-    return 0;
+cout<<c2<<endl;
+cout<<c3<<endl;
+ return 0;
 
 }
