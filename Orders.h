fd6fb075155ed4d3ea5cac94cda7f4701112a7cd
Joseph EL Bitar <joebitar06@gmail.com>
2025-10-01 03:12:02 -0400

    Added Part 3 skeleton: Orders.h

---
+++ Orders.h
@@ -0,0 +1,46 @@
+#ifndef ORDERS_H
+#define ORDERS_H
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+// ===== Base Order =====
+class Order {
+public:
+    Order();
+    virtual ~Order();
+
+    virtual bool validate() = 0;
+    virtual void execute() = 0;
+
+    // For printing orders
+    friend std::ostream& operator<<(std::ostream& os, const Order& order);
+
+protected:
+    std::string description;
+    std::string effect;
+};
+
+// ===== Deploy Order (example subclass) =====
+class Deploy : public Order {
+public:
+    Deploy();
+    bool validate() override;
+    void execute() override;
+};
+
+// ===== OrdersList =====
+class OrdersList {
+private:
+    std::vector<Order*> orders;
+
+public:
+    OrdersList();
+    ~OrdersList();
+
+    void add(Order* order);
+    void print() const;
+};
+
+#endif // ORDERS_H
