949f8f1cb39e033347afdc5c342592c6e11e788d
Achraf <chenitiachraf08@gmail.com>
2025-10-05 13:40:46 -0400

    Removed exe files and adjusted .sh files to run and delete afterwards

--- run_map_driver.sh
+++ run_map_driver.sh
@@ -4,6 +4,10 @@ g++ -std=c++17 -o MapDriver MapDriver.cpp Models/Map.cpp
 if [[ $? -eq 0 ]]; then
     echo "🚀 Running MapDriver..."
     ./MapDriver
+    
+    echo "🧹 Cleaning up executable..."
+    rm -f MapDriver
+    echo "✅ Cleanup complete!"
 else
     echo "❌ Compilation failed!"
 fi
--- run_player_driver.sh
+++ run_player_driver.sh
@@ -4,6 +4,10 @@ g++ -std=c++17 -o PlayerDriver PlayerDriver.cpp Models/Player.cpp
 if [[ $? -eq 0 ]]; then
     echo "🚀 Running MapDriver..."
     ./PlayerDriver
+
+    echo "🧹 Cleaning up executable..."
+    rm -f PlayerDriver
+    echo "✅ Cleanup complete!"
 else
     echo "❌ Compilation failed!"
 fi
